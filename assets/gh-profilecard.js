e593077df6d8097d48e09f4cf09eae5295ad3d94
Lyzev <lyzev.info@gmail.com>
2022-03-26 18:29:22 +0100

    added discord embed meta data to every .html

--- assets/gh-profilecard.js
+++ assets/gh-profilecard.js
@@ -4,66 +4,66 @@ for (let i = 0; i < cards.length; i++) {
 }
 
 async function generateProfilecard(user, container) {
-    const url = 'https://api.github.com/users/' + user;
-    const request = new XMLHttpRequest();
-    request.open("GET", url, false);
-    request.send();
+    const url = 'https://api.github.com/users/' + user
+    const request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
 
     const data = JSON.parse(request.responseText)
 
-    let card = document.createElement('div');
+    let card = document.createElement('div')
     card.style.borderRadius = '10px'
     card.style.padding = '10px'
     card.style.margin = '10px'
     card.style.height = 'fit-content'
     card.style.width = 'fit-content'
     card.style.background = '#161616'
-    card.style.display = 'flex';
+    card.style.display = 'flex'
     card.style.minWidth = '450px'
 
-    let imgContainer = document.createElement('div');
+    let imgContainer = document.createElement('div')
 
-    let img = document.createElement('img');
-    img.src = data.avatar_url;
+    let img = document.createElement('img')
+    img.src = data.avatar_url
     img.style.borderRadius = '50%'
     img.height = 80
     img.width = 80
     imgContainer.appendChild(img)
 
-    let dataContainer = document.createElement('div');
+    let dataContainer = document.createElement('div')
     dataContainer.style.marginLeft = '10px'
     dataContainer.style.display = 'flex'
     dataContainer.style.flexDirection = 'column'
     dataContainer.style.alignItems = 'flex-start'
 
-    let name = document.createElement('a');
+    let name = document.createElement('a')
     name.style.fontSize = '20px'
     name.href = data.html_url
     name.target = '_blank"'
     name.innerHTML = data.login
 
-    let desc = document.createElement('p');
+    let desc = document.createElement('p')
     desc.style.fontSize = '12px'
     desc.style.color = '#AAAAAA'
     desc.style.margin = '0'
     desc.innerHTML = data.bio
 
-    let statsContainer = document.createElement('div');
+    let statsContainer = document.createElement('div')
     statsContainer.style.display = 'flex'
 
-    let statsContainer1 = document.createElement('div');
+    let statsContainer1 = document.createElement('div')
     statsContainer1.style.margin = '5px'
     statsContainer1.style.display = 'flex'
     statsContainer1.style.flexDirection = 'column'
     statsContainer1.style.alignItems = 'center'
     statsContainer1.style.justifyContent = 'center'
-    let followers = document.createElement('a');
+    let followers = document.createElement('a')
     followers.style.fontSize = '16px'
     followers.style.margin = '0'
     followers.href = data.html_url + '?tab=followers'
     followers.target = '_blank"'
     followers.innerHTML = data.followers
-    let followerLabel = document.createElement('p');
+    let followerLabel = document.createElement('p')
     followerLabel.innerHTML = 'Followers'
     followerLabel.style.fontSize = '12px'
     followerLabel.style.color = '#AAAAAA'
@@ -71,19 +71,19 @@ async function generateProfilecard(user, container) {
     statsContainer1.appendChild(followers)
     statsContainer1.appendChild(followerLabel)
 
-    let statsContainer2 = document.createElement('div');
+    let statsContainer2 = document.createElement('div')
     statsContainer2.style.margin = '5px'
     statsContainer2.style.display = 'flex'
     statsContainer2.style.flexDirection = 'column'
     statsContainer2.style.alignItems = 'center'
     statsContainer2.style.justifyContent = 'center'
-    let following = document.createElement('a');
+    let following = document.createElement('a')
     following.style.fontSize = '16px'
     following.style.margin = '0'
     following.href = data.html_url + '?tab=following'
     following.target = '_blank"'
     following.innerHTML = data.following
-    let followingLabel = document.createElement('p');
+    let followingLabel = document.createElement('p')
     followingLabel.innerHTML = 'Following'
     followingLabel.style.fontSize = '12px'
     followingLabel.style.color = '#AAAAAA'
@@ -91,20 +91,20 @@ async function generateProfilecard(user, container) {
     statsContainer2.appendChild(following)
     statsContainer2.appendChild(followingLabel)
 
-    let statsContainer3 = document.createElement('div');
+    let statsContainer3 = document.createElement('div')
     statsContainer3.style.margin = '5px'
     statsContainer3.style.margin = '5px'
     statsContainer3.style.display = 'flex'
     statsContainer3.style.flexDirection = 'column'
     statsContainer3.style.alignItems = 'center'
     statsContainer3.style.justifyContent = 'center'
-    let rep = document.createElement('a');
+    let rep = document.createElement('a')
     rep.style.fontSize = '16px'
     rep.style.margin = '0'
     rep.href = data.html_url + '?tab=repositories'
     rep.target = '_blank"'
     rep.innerHTML = data.public_repos
-    let repLabel = document.createElement('p');
+    let repLabel = document.createElement('p')
     repLabel.innerHTML = 'Repositories'
     repLabel.style.fontSize = '12px'
     repLabel.style.color = '#AAAAAA'
--- assets/gh-repocard.js
+++ assets/gh-repocard.js
@@ -2,10 +2,10 @@ const cache = new Map()
 
 const repocards = document.getElementsByTagName('gh-repocards')
 for (let i = 0; i < repocards.length; i++) {
-    const url = 'https://api.github.com/users/' + repocards[i].getAttribute('user') + '/repos';
-    const request = new XMLHttpRequest();
-    request.open("GET", url, false);
-    request.send();
+    const url = 'https://api.github.com/users/' + repocards[i].getAttribute('user') + '/repos'
+    const request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
     const data = JSON.parse(request.responseText)
     for (let i = 0; i < data.length; i++) {
         cache.set(data[i].full_name.toUpperCase(), data[i])
@@ -31,66 +31,66 @@ for (let i = 0; i < repocard.length; i++) {
 async function generateRepocard(user, repo, container) {
     let data = cache.get((user + "/" + repo).toUpperCase())
     if (data == null) {
-        const url = 'https://api.github.com/repos/' + user + '/' + repo;
-        const request = new XMLHttpRequest();
-        request.open("GET", url, false);
-        request.send();
+        const url = 'https://api.github.com/repos/' + user + '/' + repo
+        const request = new XMLHttpRequest()
+        request.open("GET", url, false)
+        request.send()
         data = JSON.parse(request.responseText)
     }
 
-    const card = document.createElement('div');
+    const card = document.createElement('div')
     card.style.borderRadius = '10px'
     card.style.padding = '10px'
     card.style.margin = '10px'
     card.style.height = 'fit-content'
     card.style.width = 'fit-content'
     card.style.background = '#161616'
-    card.style.display = 'flex';
+    card.style.display = 'flex'
     card.style.minWidth = '450px'
 
-    const imgContainer = document.createElement('div');
+    const imgContainer = document.createElement('div')
 
-    const img = document.createElement('img');
-    img.src = data.owner.avatar_url;
+    const img = document.createElement('img')
+    img.src = data.owner.avatar_url
     img.style.borderRadius = '50%'
     img.height = 80
     img.width = 80
     imgContainer.appendChild(img)
 
-    const dataContainer = document.createElement('div');
+    const dataContainer = document.createElement('div')
     dataContainer.style.marginLeft = '10px'
     dataContainer.style.display = 'flex'
     dataContainer.style.flexDirection = 'column'
     dataContainer.style.alignItems = 'flex-start'
 
-    const name = document.createElement('a');
+    const name = document.createElement('a')
     name.style.fontSize = '20px'
     name.href = data.html_url
     name.target = '_blank"'
     name.innerHTML = data.full_name
 
-    const desc = document.createElement('p');
+    const desc = document.createElement('p')
     desc.style.fontSize = '12px'
     desc.style.color = '#AAAAAA'
     desc.style.margin = '0'
     desc.innerHTML = data.description
 
-    const statsContainer = document.createElement('div');
+    const statsContainer = document.createElement('div')
     statsContainer.style.display = 'flex'
 
-    const statsContainer1 = document.createElement('div');
+    const statsContainer1 = document.createElement('div')
     statsContainer1.style.margin = '5px'
     statsContainer1.style.display = 'flex'
     statsContainer1.style.flexDirection = 'column'
     statsContainer1.style.alignItems = 'center'
     statsContainer1.style.justifyContent = 'center'
-    const forks = document.createElement('a');
+    const forks = document.createElement('a')
     forks.style.fontSize = '16px'
     forks.style.margin = '0'
     forks.href = data.html_url + '/network/members'
     forks.target = '_blank"'
     forks.innerHTML = data.forks_count
-    const forksLabel = document.createElement('p');
+    const forksLabel = document.createElement('p')
     forksLabel.innerHTML = 'Forks'
     forksLabel.style.fontSize = '12px'
     forksLabel.style.color = '#AAAAAA'
@@ -98,19 +98,19 @@ async function generateRepocard(user, repo, container) {
     statsContainer1.appendChild(forks)
     statsContainer1.appendChild(forksLabel)
 
-    const statsContainer2 = document.createElement('div');
+    const statsContainer2 = document.createElement('div')
     statsContainer2.style.margin = '5px'
     statsContainer2.style.display = 'flex'
     statsContainer2.style.flexDirection = 'column'
     statsContainer2.style.alignItems = 'center'
     statsContainer2.style.justifyContent = 'center'
-    const stars = document.createElement('a');
+    const stars = document.createElement('a')
     stars.style.fontSize = '16px'
     stars.style.margin = '0'
     stars.href = data.html_url + '/stargazers'
     stars.target = '_blank"'
     stars.innerHTML = data.stargazers_count
-    const starslabel = document.createElement('p');
+    const starslabel = document.createElement('p')
     starslabel.innerHTML = 'Stars'
     starslabel.style.fontSize = '12px'
     starslabel.style.color = '#AAAAAA'
@@ -120,20 +120,20 @@ async function generateRepocard(user, repo, container) {
 
     let statsContainer3 = null
     if (data.language != null) {
-        statsContainer3 = document.createElement('div');
+        statsContainer3 = document.createElement('div')
         statsContainer3.style.margin = '5px'
         statsContainer3.style.margin = '5px'
         statsContainer3.style.display = 'flex'
         statsContainer3.style.flexDirection = 'column'
         statsContainer3.style.alignItems = 'center'
         statsContainer3.style.justifyContent = 'center'
-        const lang = document.createElement('a');
+        const lang = document.createElement('a')
         lang.style.fontSize = '16px'
         lang.style.margin = '0'
         lang.href = data.html_url + '/search?l=' + data.language
         lang.target = '_blank"'
         lang.innerHTML = data.language
-        const langLabel = document.createElement('p');
+        const langLabel = document.createElement('p')
         langLabel.innerHTML = 'Language'
         langLabel.style.fontSize = '12px'
         langLabel.style.color = '#AAAAAA'
@@ -159,11 +159,11 @@ async function generateRepocard(user, repo, container) {
 
 function shuffle(sourceArray) {
     for (let i = 0; i < sourceArray.length - 1; i++) {
-        const j = i + Math.floor(Math.random() * (sourceArray.length - i));
+        const j = i + Math.floor(Math.random() * (sourceArray.length - i))
 
-        const temp = sourceArray[j];
-        sourceArray[j] = sourceArray[i];
-        sourceArray[i] = temp;
+        const temp = sourceArray[j]
+        sourceArray[j] = sourceArray[i]
+        sourceArray[i] = temp
     }
-    return sourceArray;
+    return sourceArray
 }
