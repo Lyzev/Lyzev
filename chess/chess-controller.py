8126b89f9cd0bebfd675a54dd77d0fe8c1279cd0
Lyzev <69327579+Lyzev@users.noreply.github.com>
2025-02-19 00:18:57 +0100

    feat: include timestamp in last move display

--- chess/chess-controller.py
+++ chess/chess-controller.py
@@ -1,3 +1,4 @@
+import datetime
 import chess
 import os
 import argparse
@@ -60,13 +61,10 @@ def board_to_markdown(board, legal_moves):
         board_md += "|\n"
     board_md += f"\n**Next move:** {"White" if board.turn else "Black"}\n"
 
-    board_md += "\n**Last move:**\n"
     if len(board.move_stack) > 0:
+        board_md += "\n**Last move:**\n"
         last_move = board.peek()
-        board_md += f"\n{chess.square_name(last_move.from_square)} -> {chess.square_name(last_move.to_square)}\n"
-        board_md += f"\nDate: {last_move}\n"
-    else:
-        board_md += "\nNone\n"
+        board_md += f"\n{chess.square_name(last_move.from_square)} -> {chess.square_name(last_move.to_square)} | {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
 
     wdl = stockfish.get_wdl_stats()
     win_percentage = (wdl[0] + wdl[1] / 2) / sum(wdl) * 100
