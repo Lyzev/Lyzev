d213234ee131291bea58775b6516940ed30eea30
Lyzev <69327579+Lyzev@users.noreply.github.com>
2025-02-20 12:56:26 +0100

    Adjust Stockfish ELO rating range and limit top moves displayed

--- chess/chess-best-move.py
+++ chess/chess-best-move.py
@@ -5,14 +5,14 @@ import random
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 stockfish = Stockfish(path=script_dir + "/stockfish-ubuntu-x86-64-avx2", depth=14, parameters={"Threads": 2, "Minimum Thinking Time": 3})
-stockfish.set_elo_rating(random.randint(850, 1350))
+stockfish.set_elo_rating(random.randint(1050, 1350))
 
 if __name__ == "__main__":
     filepath = os.path.join(script_dir, "board.fen")
     if os.path.exists(filepath):
         with open(filepath, "r") as file:
             stockfish.set_fen_position(file.read().strip())
-            print(random.choice(stockfish.get_top_moves(5))["Move"])
+            print(random.choice(stockfish.get_top_moves(2))["Move"])
             exit(0)
     print("No board.fen file found")
     exit(1)
